66d58997042a9d06d112411df81114b79b8ad260
abusaidshabib <abusaidshabib712@gmail.com>
2022-10-27 08:00:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+const topics = require('./data/topics.json');
+
+app.get('/', (req, res)=>{
+    res.send('ReactData API Running');
+});
+
+app.get('/topics', (req, res) =>{
+    res.send(topics)
+})
+
+
+
+app.listen(port, () => {
+    console.log('CodEdu server running');
+})
